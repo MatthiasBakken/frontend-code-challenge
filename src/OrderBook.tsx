f59e81d43779aebf74de527e3853150bcf9af434
Matthias Bakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-06-18 05:42:38 -0700

    added cypress for end to end test and added dynamic styles for showing size of trades in element with color

--- src/OrderBook.tsx
+++ src/OrderBook.tsx
@@ -7,6 +7,17 @@ const BOOK = "book__";
 
 const OrderBook: React.FC<TOrderBookStream> = ( { buy, sell } ) => {
 
+  let totalBuy: number = 0.00;
+  let totalSell: number = 0.00;
+
+  buy.forEach( order => {
+    totalBuy += parseFloat( order.price ) * parseFloat( order.amount );
+  } );
+
+  sell.forEach( order => {
+    totalSell += parseFloat( order.price ) * parseFloat( order.amount );
+  } );
+
   return (
     <div className={`${BOOK}container`}>
       <div className={`${BOOK}content-container`}>
@@ -19,7 +30,7 @@ const OrderBook: React.FC<TOrderBookStream> = ( { buy, sell } ) => {
             <h4>VOL (USD)</h4>
           </div>
           {buy.map( ( buyOrder ) => (
-            <div className={`${BOOK}price-amt-vol`}>
+            <div className={`${BOOK}price-amt-vol`} style={{background: `linear-gradient(to right, #14202c ${100 - (( parseFloat( buyOrder.price ) * parseFloat( buyOrder.amount ) ) / totalBuy * 100)}%, maroon ${( parseFloat( buyOrder.price ) * parseFloat( buyOrder.amount ) ) / totalBuy * 100}%)`}}>
               <p className={'buy'}>{parseFloat(buyOrder.price).toFixed(2)}</p>
               <p>{buyOrder.amount}</p>
               <p>{( parseFloat( buyOrder.price ) * parseFloat( buyOrder.amount ) ).toFixed( 3 )}</p>
@@ -35,7 +46,8 @@ const OrderBook: React.FC<TOrderBookStream> = ( { buy, sell } ) => {
             <h4>VOL (USD)</h4>
           </div>
           {sell.map( ( sellOrder ) => (
-            <div className={`${BOOK}price-amt-vol`}>
+            <div className={`${BOOK}price-amt-vol`} style={
+              { background: `linear-gradient(to right, #14202c ${100 - (( parseFloat( sellOrder.price ) * parseFloat( sellOrder.amount ) ) / totalSell * 100)}%, #397169 ${( parseFloat( sellOrder.price ) * parseFloat( sellOrder.amount ) ) / totalSell * 100}%)` }}>
               <p className="sell">{parseFloat(sellOrder.price).toFixed(2)}</p>
               <p>{sellOrder.amount}</p>
               <p>{( parseFloat( sellOrder.price ) * parseFloat( sellOrder.amount ) ).toFixed( 3 )}</p>
